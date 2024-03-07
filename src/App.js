e71751a7c3cae2685f10367ef0a29a44e0186389
Ilia Shved <ilia83ivanovich@gmail.com>
2024-03-07 23:54:46 +0500

    instal redux/toolkit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './scss/app.scss';
 import Header from './components/Header';
 import { Routes, Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Error404 from './pages/Error404';
 import Home from './pages/Home';
